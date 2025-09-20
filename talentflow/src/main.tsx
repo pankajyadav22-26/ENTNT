4e47df4d07ac40ba2688c825309c7f4d9d95451a
pankajyadav22-26 <142982981+pankajyadav22-26@users.noreply.github.com>
2025-09-21 02:16:16 +0530

    Stage: Updated main.tsx For Contexts

--- talentflow/src/main.tsx
+++ talentflow/src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
@@ -14,7 +18,11 @@ async function enableMocking() {
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </React.StrictMode>
   );
 });
