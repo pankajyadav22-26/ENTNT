5a076ed1d0cba7784ab1564d1bfc0a3c94ef7362
pankajyadav22-26 <142982981+pankajyadav22-26@users.noreply.github.com>
2025-09-21 01:58:03 +0530

    Stage: Added Schema For Candidate

--- talentflow/src/lib/db.ts
+++ talentflow/src/lib/db.ts
@@ -9,13 +9,23 @@ export interface Job {
   order: number;
 }
 
+export interface Candidate {
+  id: string;
+  name: string;
+  email: string;
+  jobId: string;
+  stage: 'applied' | 'screen' | 'tech' | 'offer' | 'hired' | 'rejected';
+}
+
 export class TalentFlowDB extends Dexie {
   jobs!: Table<Job>;
+  candidates!: Table<Candidate>;
 
   constructor() {
     super('talentFlowDatabase');
     this.version(1).stores({
-      jobs: '++id, slug, status, order'
+      jobs: '++id, slug, status, order',
+      candidates: '++id, jobId, stage',
     });
   }
 }
